eed76a56c04e39293d4780918844d55dad507d10
Lonerr7 <krabik2342@gmail.com>
2021-10-03 14:54:24 +0300

    Add burger-menu functionality

--- js/script.js
+++ js/script.js
@@ -6,7 +6,8 @@
 const headerMenu = document.querySelector('.header__menu');
 
 // Burger-icon animation
-const burgerIcon = document.querySelector('.header__burger-icon');
+const burgerIcon = document.querySelector('.header__burger');
+const body = document.querySelector('body');
 
 // Smooth scrolling
 const linkDown = document.querySelector('.section-hero__scroll');
@@ -21,16 +22,6 @@ const allSections = document.querySelectorAll('.section');
 if (window.visualViewport.width > 524) {
     headerMenu.classList.remove('header__menu_invisible');
 }
-
-//===========================================================
-
-//* Icon-burger animation
-
-burgerIcon.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    burgerIcon.classList.toggle('header__burger-icon_active');
-})
 
 //===========================================================
 
@@ -51,4 +42,29 @@ navLinks.addEventListener('click', (e) => {
     if (e.target.classList.contains('header__link')) {
         document.querySelector(id).scrollIntoView({behavior: 'smooth'});
     }
+
+    if (burgerIcon.classList.contains('header__burger-icon_active')) {
+        body.classList.remove('lock');
+        headerMenu.classList.remove('header__menu_active');
+        burgerIcon.classList.remove('header__burger-icon_active');
+    }
 });
+
+//===========================================================
+
+//* Menu-burger animmation
+
+// Brurger-icon animation
+burgerIcon.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const clicked = e.target.closest('.header__burger');
+
+    if(!clicked) return;
+    clicked.classList.toggle('header__burger-icon_active');
+
+    // Menu-burger animation
+    body.classList.toggle('lock');
+    headerMenu.classList.toggle('header__menu_active');
+})
+
